11587ce75cfe9415899c6bf6121f2341c5cfd17c
LINDSEY-KIM <88068327+LINDSEY-KIM@users.noreply.github.com>
2022-04-10 23:31:18 +0900

    limit character input box exam

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("form input"),
+counter = document.querySelector("form .counter"),
+maxLength = input.getAttribute("maxlength");
+input.onkeyup = ()=>{
+  counter.innerText = maxLength - input.value.length;
+}
